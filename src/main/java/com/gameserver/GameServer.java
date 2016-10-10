4e4e73eebec911637a41d6cc3531cfdc6a104648
Yuri Ivanov <yura.ivanov@gmail.com>
2016-10-11 00:32:51 +0200

    add simple rest

--- src/main/java/com/gameserver/GameServer.java
+++ src/main/java/com/gameserver/GameServer.java
@@ -1,7 +1,24 @@
 package com.gameserver;
 
-/**
- * Created by yivanov on 26.09.16.
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
 public class GameServer {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(GameServer.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+
 }
