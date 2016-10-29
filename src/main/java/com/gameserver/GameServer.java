30862f707752f804788019ef24616bffd61189a8
Yuri Ivanov <yura.ivanov@gmail.com>
2016-10-30 01:13:17 +0200

    other changes in jdbc

--- src/main/java/com/gameserver/GameServer.java
+++ src/main/java/com/gameserver/GameServer.java
@@ -1,24 +1,89 @@
 package com.gameserver;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ContextResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.jndi.JndiTemplate;
 
-import java.util.Arrays;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class GameServer {
+public class GameServer extends SpringBootServletInitializer implements CommandLineRunner{
+    @Value("${connection.jndiName}")
+    String connectionJndiName;
+    @Value("${connection.driver-class-name}")
+    String connectionDriverClassName;
+    @Value("${connection.url}")
+    String connectionURL;
+    @Value("${connection.username}")
+    String connectionUserName;
+    @Value("${connection.password}")
+    String connectionPassword;
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(GameServer.class, args);
+    }
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        //during startup
+    }
+
+
+    @Lazy
+    @Bean(destroyMethod="")
+    public DataSource jndiDataSource() throws IllegalArgumentException, NamingException {
+        JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
+        bean.setJndiName("java:comp/env/"+connectionJndiName);
+        bean.setProxyInterface(DataSource.class);
+        bean.setLookupOnStartup(false);
+        bean.afterPropertiesSet();
+        return (DataSource) bean.getObject();
+    }
+
+    @Bean
+    public TomcatEmbeddedServletContainerFactory tomcatFactory() {
+        return new TomcatEmbeddedServletContainerFactory() {
+
+            @Override
+            protected TomcatEmbeddedServletContainer getTomcatEmbeddedServletContainer(
+                    Tomcat tomcat) {
+                tomcat.enableNaming();
+                return super.getTomcatEmbeddedServletContainer(tomcat);
+            }
+
+            @Override
+            protected void postProcessContext(Context context) {
+                ContextResource resource = new ContextResource();
+                resource.setName(connectionJndiName);
+                resource.setType(DataSource.class.getName());
+                resource.setProperty("factory", "org.apache.tomcat.jdbc.pool.DataSourceFactory");
+                resource.setProperty("driverClassName", connectionDriverClassName);
+                resource.setProperty("url", connectionURL);
+                resource.setProperty("password", connectionPassword);
+                resource.setProperty("username", connectionUserName);
+
+                context.getNamingResources().addResource(resource);
+            }
+        };
     }
 
 }
--- src/main/java/com/gameserver/rest/HelloController.java
+++ src/main/java/com/gameserver/rest/HelloController.java
@@ -1,14 +1,37 @@
 package com.gameserver.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @RestController
 public class HelloController {
+    /*
+    @Autowired
+    private DataSource ds;
+    */
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
 
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/dbtest")
+    public String dbTest() throws SQLException {
+        StringBuffer buffer = new StringBuffer();
+        jdbcTemplate.query("select * from test", (rs, rowNum)-> (rs.getString("text")) ).forEach(text -> buffer.append(text) );
+        return buffer.toString();
     }
 
 }
